26b0fe091c6d6c5c5f4163e8a292fe621493ffef
GXC2356 <gxcdev2356@gmail.com>
2025-01-03 15:11:55 +0200

    vayu: GXCUI adapt

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -11,21 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common Aosp configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common Lineage configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_DISABLE_EPPE := true
-
-# SkylineUI Maintainer Flags
-SKYLINEUI_MAINTAINER := GXC2356
-CUSTOM_BUILD_TYPE := OFFICIAL
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
