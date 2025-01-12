d9d452f459fdcb52eb4789db04627ef640247ab8
GXC2356 <gxcdev2356@gmail.com>
2025-01-12 19:12:23 +0200

    [TMP] vayu: vendorsetup.sh: remove qti_headers from vendor_flare

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,3 +10,6 @@ git clone --depth=1 https://github.com/aospa-x3pro/kernel_msm-4.14.git -b vauxit
 rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-22.0
 rm -rf hardware/xiaomi/megvii
+
+# Rename conflicting qti_kernel_headers in source
+sed -i 's/"qti_kernel_headers"/"qti_kernel_headers_old"/g' vendor/flare/build/soong/Android.bp
