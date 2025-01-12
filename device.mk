kuroringo90/device_xiaomi_vayu

vayu: Switch to if product exists at sign keys to prevent errors

device.mk
@@ -38,7 +38,7 @@ $(call inherit-product-if-exists, vendor/dolby/dolby.mk)
 
 
 # Inherit from release keys
-$(call inherit-product, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/extra/product.mk)
 
 # Audio
 PRODUCT_PACKAGES += \

GXC2356/device_xiaomi_vayu-gxcui

device.mk
@@ -34,7 +34,7 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from release keys
-$(call inherit-product, vendor/keys/product.mk)
+$(call inherit-product-if-exists, vendor/keys/product.mk)
 
 # Audio
 PRODUCT_PACKAGES += \