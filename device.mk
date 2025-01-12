kuroringo90/device_xiaomi_vayu

vayu: Inherit keys for signing builds
Signed-off-by: 0mar99 <omarag9099@gmail.com>

device.mk
@@ -37,6 +37,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 $(call inherit-product-if-exists, vendor/dolby/dolby.mk)
 
 
+# Inherit from release keys
+$(call inherit-product, vendor/extra/product.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \

GXC2356/device_xiaomi_vayu-gxcui

device.mk
@@ -33,6 +33,9 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # Dynamic partitions setup
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Inherit from release keys
+$(call inherit-product, vendor/keys/product.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \